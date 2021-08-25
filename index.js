7d13fe9c0913b20f3235c241f856d0e01f0c2f30
jesse trujillo <33822347+jtrujillo707@users.noreply.github.com>
2021-08-25 22:41:13 +0000

    initial code

---
+++ index.js
@@ -0,0 +1,15 @@
+let myhttp = require("http");
+
+let myserver = myhttp.createServer( 
+
+  function( myrequest , myresponse ){
+    console.log(myrequest.url);
+
+    myresponse.writeHead(200, { "content-type": "text/plain" });
+    myresponse.end( " Hello World\n");
+  }
+
+);
+myserver.listen(8080,"0.0.0.0");
+
+console.log("server has started");
