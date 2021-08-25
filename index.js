c8f03f5b2f25d336fd213a26bc65d969a7fdeda8
jesse trujillo <33822347+jtrujillo707@users.noreply.github.com>
2021-08-25 22:42:57 +0000

    cleaned up

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ let myserver = myhttp.createServer(
     myresponse.writeHead(200, { "content-type": "text/plain" });
     myresponse.end( " Hello World\n");
   }
-
-);
+ );
 myserver.listen(8080,"0.0.0.0");
 
 console.log("server has started");
